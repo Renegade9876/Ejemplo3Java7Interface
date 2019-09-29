ae8ed63286538e26ec7e338832c3acb2ce4b275e
Renegade <rene.paneda.sande@gmail.com>
2019-09-30 01:40:15 +0200

    Commit inicial del repositorio Ejemplo3Java7Interface

---
+++ src/main/java/com/renegade/Ejemplo3Java7Interface.java
@@ -0,0 +1,18 @@
+package com.renegade;
+
+public class Ejemplo3Java7Interface {
+
+	// Utilizando clases anónimas con Java 7.
+	public double sumar(double d1, double d2) {
+		return d1+d2;
+	}
+	
+	public static void main(String[] args) {
+		Ejemplo3Java7Interface ejemplo3 = new Ejemplo3Java7Interface();
+		
+		// Utilizando clases anónimas con Java 7.
+		System.out.println(ejemplo3.sumar(10, 20));
+		
+	}
+
+}
